9f48880271eb74e3bf0b0686cb79402d9dfd331d
krochet94 <carljasoncainaragoncillo@gmail.com>
2022-07-28 04:30:07 +0800

    update dependencies

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use("/api/v1", require("./routes/payment"));
 //created after building frontend
 if((process.env.NODE_ENV || "").trim() === "PRODUCTION") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 } 
